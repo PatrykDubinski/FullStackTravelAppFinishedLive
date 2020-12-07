52dc1c3632e64575a41e5e055442308fb5a206a7
PatrykDubinski <dubinskipatryk99@gmail.com>
2020-12-07 19:06:52 +0100

    Fix app

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 app.use("/auth", authRoutes);
